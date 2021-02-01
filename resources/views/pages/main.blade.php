696298c0ae54d6a486fd0e72b874756c5da94f2c
laybelmeyden <trycollens@gmail.com>
2021-02-01 18:57:57 +0300

    fixed/news

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -162,8 +162,8 @@
     <div class="col s12 m4">
       <div class="card">
         <div class="card-image">
-          <img src="storage/{{ $new->img }}">
-          <span class="card-title">{!! Str::limit($new -> title, 15) !!}</span>
+          <img src="/storage/{{ $new->img }}">
+          <span class="card-title">{!! Str::limit($new -> title, 25) !!}</span>
         </div>
         <div class="card-content">
           <p>{!! Str::limit($new -> body, 50) !!}</p>
